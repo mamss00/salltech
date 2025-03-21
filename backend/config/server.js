488e0c82aa67d57c7e3c795ca90054ef64b2cad5
mamss00 <sallmamadou91@gmail.com>
2025-03-21 21:41:07 +0100

    try fix server 2

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,11 +4,6 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://api.sall.technology'),
-  admin: {
-    url: '/admin',
-    autoOpen: false,
-  },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
