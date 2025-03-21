8753c7642d4ba465e27683689a7cfbbfd57bc68a
mamss00 <sallmamadou91@gmail.com>
2025-03-21 21:22:08 +0100

    try fix server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,8 +1,14 @@
+// backend/config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  admin: {
+    autoOpen: true,
+    url: '/admin',
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
