a2830f0455800a618e89b03f96aaabe42936f99f
Mamadou Sall <sallmamadou91@gmail.com>
2025-05-02 00:55:13 +0200

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'api.sall.technology'],
   },
   webpack(config) {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
