d6c8df6949ec87073bc5d2b032514ada84e02e14
Mamadou Sall <sallmamadou91@gmail.com>
2025-05-02 00:59:27 +0200

    fix next

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'api.sall.technology'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.sall.technology',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'api.sall.technology',
+        pathname: '/**',
+      }
+    ],
   },
   webpack(config) {
     config.resolve.alias['@'] = path.join(__dirname, 'src');
