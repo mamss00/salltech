981763adaa814f68e1c1af172b87f39229e6e0b8
Mamadou Sall <sallmamadou91@gmail.com>
2025-05-02 02:11:00 +0200

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -15,7 +17,11 @@ const nextConfig = {
         pathname: '/**',
       }
     ],
-  }
+  },
+  webpack(config) {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
 }
 
 module.exports = nextConfig;
